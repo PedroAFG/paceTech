e1db9ee614a59c3cb02c23efa033f46ddc655292
PedroAFG <66686441+PedroAFG@users.noreply.github.com>
2022-08-19 19:07:00 -0300

    Add files via upload

---
+++ Script_Missao2.sql
@@ -0,0 +1,30 @@
+/* Créer la table Employee */
+CREATE TABLE Employee (
+    Employee_id int AUTO_INCREMENT PRIMARY KEY,
+    First_name VARCHAR(50),
+	Last_name VARCHAR(50),
+	Salary int,
+	Joining_date Date,
+    Departement  VARCHAR(50) 
+);
+
+
+/* Insérer des lignes dans la table Employee */
+INSERT INTO Employee (Employee_id, First_name, Last_name, Salary, Joining_date, Departement) VALUES (1, 'Bob', 'Kinto', 5000, "2019-01-20", "Finance");
+INSERT INTO Employee (Employee_id, First_name, Last_name, Salary, Joining_date, Departement) VALUES (2, 'Jerry', 'Kansxo', 8500, "2019-01-15", "IT");
+INSERT INTO Employee (Employee_id, First_name, Last_name, Salary, Joining_date, Departement) VALUES (3, 'Philip', 'Jose', 6000, "2019-02-05", "Banking");
+INSERT INTO Employee (Employee_id, First_name, Last_name, Salary, Joining_date, Departement) VALUES (4, 'John', 'Abraham', 5000, "2019-02-25", "Insurance");
+INSERT INTO Employee (Employee_id, First_name, Last_name, Salary, Joining_date, Departement) VALUES (5, 'Michael', 'Mathew', 4500, "2019-02-28", "Finance");
+INSERT INTO Employee (Employee_id, First_name, Last_name, Salary, Joining_date, Departement) VALUES (6, 'Alex', 'chreketo', 7000, "2019-05-10", "IT");
+INSERT INTO Employee (Employee_id, First_name, Last_name, Salary, Joining_date, Departement) VALUES (7, 'Yohan', 'Soso', 4000, "2019-06-20", "Banking");
+
+
+// RESPOSTAS DA MISSÃO: 
+
+1) select * from Employee order by Salary DESC;
+
+2) select DISTINCT Departement from Employee;
+
+3) select AVG(Salary) from Employee;
+
+4) select First_name, Last_name, Salary from Employee where salary > '5714,2857' ;
